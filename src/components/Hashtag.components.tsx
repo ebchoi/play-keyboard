9539fe284d57f0437d6cb55e980bed0659a901eb
kwanhee <yrats1234@gmail.com>
2022-10-06 18:15:33 +0900

    typeScript 고침

--- src/components/Hashtag.components.tsx
+++ src/components/Hashtag.components.tsx
@@ -1,7 +1,7 @@
 import { colors } from '../styles/theme';
 import styled from 'styled-components';
 
-function Hashtag({ keyword }) {
+function Hashtag({ keyword }: { keyword: string }) {
   return <Span>{keyword}</Span>;
 }
 export default Hashtag;
